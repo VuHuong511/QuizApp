e38cdc62470b8f274e3eafe53db20307a67244ed
VuHuong <vuhuong51101@gmail.com>
2023-06-10 10:33:11 +0700

    add functionality to start the quiz

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import QuizScreen from "./QuizScreen";
 
 const App = () => {
+  const [quizStarted, setQuizStarted] = useState(false);
+  const [playerName, setPlayerName] = useState("");
+  const [nameError, setNameError] = useState("");
+
+  const handleStartQuiz = () => {
+    if (playerName.trim() === "") {
+      setNameError("Please enter your name");
+    } else {
+      setQuizStarted(true);
+    }
+  };
+
+  const handleNameChange = (event) => {
+    setPlayerName(event.target.value);
+    setNameError("");
+  };
+
   return (
     <div>
       <h1>Quiz Application</h1>
-      <QuizScreen />
+      {!quizStarted ? (
+        <div>
+          <label htmlFor="nameInput">Enter Your Name:</label>
+          <input
+            type="text"
+            id="nameInput"
+            value={playerName}
+            onChange={handleNameChange}
+            required
+          />
+          <span className="error">{nameError}</span>
+          <button onClick={handleStartQuiz}>Start Quiz</button>
+        </div>
+      ) : (
+        <QuizScreen playerName={playerName} />
+      )}
     </div>
   );
 };
